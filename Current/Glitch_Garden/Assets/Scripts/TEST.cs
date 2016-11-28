592ff0f01bc58cc973cc1a9f0f1b5ad3cf59fdd5
Hugh Healy <Hugh.Healy@gmail.com>
2016-11-28 23:02:19 +0000

    PlayerPrefs

---
+++ Current/Glitch_Garden/Assets/Scripts/TEST.cs
@@ -0,0 +1,30 @@
+﻿//2h
+//28/11/2016
+//22:20
+//Whitesgate
+
+using UnityEngine;
+using System.Collections;
+
+public class TEST : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print (PlayerPrefsManager.GetMasterVolume ());
+		PlayerPrefsManager.SetMasterVolume (0.3f);
+		print (PlayerPrefsManager.GetMasterVolume ());
+
+		print (PlayerPrefsManager.IsLevelUnlocked (2));
+		PlayerPrefsManager.UnlockLevel (2);
+		print (PlayerPrefsManager.IsLevelUnlocked (2));
+
+		print (PlayerPrefsManager.GetDifficulty());
+		PlayerPrefsManager.SetDifficulty(0.2f);
+		print (PlayerPrefsManager.GetDifficulty());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
